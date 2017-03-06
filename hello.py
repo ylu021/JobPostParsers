57a12f2140a24753e85b8aad780f6d2ad1be08a7
ylu021 <yichen_0@hotmail.com>
2017-03-06 15:35:18 -0500

    try

--- hello.py
+++ hello.py
@@ -32,4 +32,6 @@ def hello(uri=None):
 	return render_template('result.html', uri=uri)	
 	
 if __name__=="__main__":
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 	app.run()
